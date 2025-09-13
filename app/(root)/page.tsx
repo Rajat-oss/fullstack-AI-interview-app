38be8bdd815ef1309a246d6fb910aff64a50a976
Rajat-oss <rajatjhade@gmail.com>
2025-09-13 09:18:50 +0530

    Refactor Firebase Admin and Client Initialization
    - Uncommented and cleaned up Firebase Admin SDK initialization code in `firebase/admin.ts`.
    - Updated Firebase Client initialization in `firebase/client.ts` to ensure proper app instance handling.
    - Refactored authentication actions in `lib/actions/auth.action.ts` to restore functionality and improve error handling.
    - Added new `lib/actions/general.action.ts` file for handling feedback-related actions.
    - Enhanced utility functions in `lib/utils.ts` for better tech logo handling and random interview cover retrieval.
    - Updated VAPI SDK import style in `lib/vapi.sdk.ts` for consistency.
    - Introduced new TypeScript definitions for VAPI messages in `types/vapi.d.ts`.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,108 @@
-import React from 'react'
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import { dummyInterviews } from '@/constants'
-import InterviewCard from '@/components/InterviewCard'
+// import React from 'react'
+// import Link from 'next/link'
+// import { Button } from '@/components/ui/button'
+// import Image from 'next/image'
+// import { dummyInterviews } from '@/constants'
+// import InterviewCard from '@/components/InterviewCard'
+// import { getInterviewsByUserId } from '@/lib/actions/general.action'
 
-const page = () => {
+// const page = async () => {
+//   const user = await getCurrentUser();
+//   const userInterviews = await getInterviewsByUserId(user?.id!);
+
+//   const hasPastInterviews = userInterviews?.length > 0;
+
+//   return (
+//     <>
+//       <section className="card-cta">
+//         <div className="flex flex-col gap-6 max-w-lg">
+//           <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+//           <p className="text-lg">
+//             Practice on real interview question & get instant feedback
+//           </p>
+
+//           <Button asChild className="btn-primary max-sm:w-full">
+//             <Link href="/interview">Start an Interview</Link>
+//           </Button>
+//         </div>
+
+//         <Image 
+//           src="/robot.png" 
+//           alt="robo-dude" 
+//           width={400} 
+//           height={400} 
+//           className="hidden lg:block" 
+//           style={{ width: 'auto', height: 'auto' }}
+//         />
+
+//       </section>
+
+//       <section className="flex flex-col gap-6 mt-8">
+//         <h2>Your Interviews</h2>
+
+//         <div className="interviews-section">
+//           {
+//             hasPastInterviews ? (
+//               userInterviews?.map((interview) => (
+//                 <InterviewCard {...interview} key={interview.id}/>
+//               ))) : (
+//                 <p>you haven&apos;t taken any interviews</p>
+//               )
+//           }
+          
+//         </div>
+//       </section>
+
+//       <section className="flex flex-col gap-6 mt-8">
+//         <h2>Take an Interview</h2>
+
+//         <div className="interviews-section">
+//         {dummyInterviews.map((interview) => (
+//             <InterviewCard {...interview} key={interview.id}/>
+//           ))}
+
+//         </div>
+//       </section>
+
+//     </>
+//   )
+// }
+
+// export default page
+
+
+
+
+import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import InterviewCard from "@/components/InterviewCard";
+
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import {
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/general.action";
+
+async function Home() {
+  const user = await getCurrentUser();
+
+  const [userInterviews, allInterview] = await Promise.all([
+    getInterviewsByUserId(user?.id!),
+    getLatestInterviews({ userId: user?.id! }),
+  ]);
+
+  const hasPastInterviews = userInterviews?.length! > 0;
+  const hasUpcomingInterviews = allInterview?.length! > 0;
+
   return (
     <>
       <section className="card-cta">
         <div className="flex flex-col gap-6 max-w-lg">
           <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
           <p className="text-lg">
-            Practice on real interview question & get instant feedback
+            Practice real interview questions & get instant feedback
           </p>
 
           <Button asChild className="btn-primary max-sm:w-full">
@@ -20,43 +110,60 @@ const page = () => {
           </Button>
         </div>
 
-        <Image 
-          src="/robot.png" 
-          alt="robo-dude" 
-          width={400} 
-          height={400} 
-          className="hidden lg:block" 
-          style={{ width: 'auto', height: 'auto' }}
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
         />
-
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
 
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id}/>
-          ))}
-
-          {/* <p>you haven&apos;t taken any interviews</p> */}
-
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
         </div>
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
-        <h2>Take an Interview</h2>
+        <h2>Take Interviews</h2>
 
         <div className="interviews-section">
-        {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id}/>
-          ))}
-
+          {hasUpcomingInterviews ? (
+            allInterview?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>There are no interviews available</p>
+          )}
         </div>
       </section>
-
     </>
-  )
+  );
 }
 
-export default page
+export default Home;
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,120 +1,416 @@
-'use server';
+// 'use server';
 
-import { db, auth } from '@/firebase/admin';
-import { cookies } from 'next/headers';
-
-
-
-const ONE_WEEK = 60 * 60 * 24 * 7;
+// import { db, auth } from '@/firebase/admin';
+// import { CollectionReference } from 'firebase-admin/firestore';
+// import { cookies } from 'next/headers';
 
 
-export async function signUp(params: SignUpParams) {
-   const {uid, name, email} = params;
 
-   try {
-    const userRecord = await db.collection('users').doc(uid).get();
+// const ONE_WEEK = 60 * 60 * 24 * 7;
 
-    if(userRecord.exists) {
-        return {
-            success: false,
-            message: 'User already exists. Please sign in instead.'
-        }
-    }
 
-    await db.collection('users').doc(uid).set({
-        name, email
-    })
+// export async function signUp(params: SignUpParams) {
+//    const {uid, name, email} = params;
 
-    return {
-        success: true,
-        message: 'Account created successfully. Please sign in.'
-    }
-   } catch (e: any) {
-        console.error('Error creating a user', e);
+//    try {
+//     const userRecord = await db.collection('users').doc(uid).get();
 
-            if(e.code === 'auth/email-already-in-use') {
-                return{
-                    success: false,
-                    message: 'This email is already in use.'
-            }
-        }
+//     if(userRecord.exists) {
+//         return {
+//             success: false,
+//             message: 'User already exists. Please sign in instead.'
+//         }
+//     }
 
-        return {
-            success: false,
-            message: 'failed to create an account'
-        }
-    }
+//     await db.collection('users').doc(uid).set({
+//         name, email
+//     })
+
+//     return {
+//         success: true,
+//         message: 'Account created successfully. Please sign in.'
+//     }
+//    } catch (e: any) {
+//         console.error('Error creating a user', e);
+
+//             if(e.code === 'auth/email-already-in-use') {
+//                 return{
+//                     success: false,
+//                     message: 'This email is already in use.'
+//             }
+//         }
+
+//         return {
+//             success: false,
+//             message: 'failed to create an account'
+//         }
+//     }
+// }
+
+// export async function setSessionCookie(idToken: string) {
+//     const cookieStore = await cookies();
+
+//     const setSessionCookie = await auth.createSessionCookie(idToken, {
+//         expiresIn: ONE_WEEK * 1000 
+//     })
+
+//     cookieStore.set('session', setSessionCookie, {
+//         maxAge: ONE_WEEK,
+//         httpOnly: true,
+//         secure: process.env.NODE_ENV === 'production',
+//         path: '/',
+//         sameSite: 'lax'
+//     })
+// }
+
+// export async function signIn(params: SignInParams) {
+//     const { email, idToken } = params;
+//     try {
+//         const userRecord = await auth.getUserByEmail(email);
+
+//         if(!userRecord) {
+//             return {
+//                 success: false,
+//                 message: 'User does not exist. Create an account instead.'
+//             }
+//         }
+
+//         await setSessionCookie(idToken);
+//     } catch (e) {
+//         console.log(e);
+
+//         return {
+//             success: false,
+//             message: 'Failed to log into an account.'
+//         }
+//     }
+// }
+
+// export async function getCurrentUser(): Promise<User | null> {
+//     const cookieStore = await cookies();
+  
+//     const sessionCookie = cookieStore.get("session")?.value;
+//     if (!sessionCookie) return null;
+  
+//     try {
+//       const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+  
+//       // get user info from db
+//       const userRecord = await db
+//         .collection("users")
+//         .doc(decodedClaims.uid)
+//         .get();
+//       if (!userRecord.exists) return null;
+  
+//       return {
+//         ...userRecord.data(),
+//         id: userRecord.id,
+//       } as User;
+//     } catch (error) {
+//       console.log(error);
+  
+//       // Invalid or expired session
+//       return null;
+//     }
+//   }
+
+// export async function isAuthenticated() {
+//     const user = await getCurrentUser();
+
+//     return !!user;
+// }
+
+// export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
+//   const interviews = await db
+//   .collection('interviews')CollectionReference<DocumentData, DocumentData>
+//   .where('userId', '==', userId)Query<DocumentData, DocumentData>
+//   .orderBy('createdAt', 'desc')
+//   .get();
+
+//   return interviews.docs.map((doc) => ({
+//     id: doc.id,
+//     ...doc.data()
+//   }))
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // "use server";
+
+// // import { auth, db } from "@/firebase/admin";
+// // import { cookies } from "next/headers";
+
+// // // Session duration (1 week)
+// // const SESSION_DURATION = 60 * 60 * 24 * 7;
+
+// // // Set session cookie
+// // export async function setSessionCookie(idToken: string) {
+// //   const cookieStore = await cookies();
+
+// //   // Create session cookie
+// //   const sessionCookie = await auth.createSessionCookie(idToken, {
+// //     expiresIn: SESSION_DURATION * 1000, // milliseconds
+// //   });
+
+// //   // Set cookie in the browser
+// //   cookieStore.set("session", sessionCookie, {
+// //     maxAge: SESSION_DURATION,
+// //     httpOnly: true,
+// //     secure: process.env.NODE_ENV === "production",
+// //     path: "/",
+// //     sameSite: "lax",
+// //   });
+// // }
+
+// // export async function signUp(params: SignUpParams) {
+// //   const { uid, name, email } = params;
+
+// //   try {
+// //     // check if user exists in db
+// //     const userRecord = await db.collection("users").doc(uid).get();
+// //     if (userRecord.exists)
+// //       return {
+// //         success: false,
+// //         message: "User already exists. Please sign in.",
+// //       };
+
+// //     // save user to db
+// //     await db.collection("users").doc(uid).set({
+// //       name,
+// //       email,
+// //       // profileURL,
+// //       // resumeURL,
+// //     });
+
+// //     return {
+// //       success: true,
+// //       message: "Account created successfully. Please sign in.",
+// //     };
+// //   } catch (error: any) {
+// //     console.error("Error creating user:", error);
+
+// //     // Handle Firebase specific errors
+// //     if (error.code === "auth/email-already-exists") {
+// //       return {
+// //         success: false,
+// //         message: "This email is already in use",
+// //       };
+// //     }
+
+// //     return {
+// //       success: false,
+// //       message: "Failed to create account. Please try again.",
+// //     };
+// //   }
+// // }
+
+// // export async function signIn(params: SignInParams) {
+// //   const { email, idToken } = params;
+
+// //   try {
+// //     const userRecord = await auth.getUserByEmail(email);
+// //     if (!userRecord)
+// //       return {
+// //         success: false,
+// //         message: "User does not exist. Create an account.",
+// //       };
+
+// //     await setSessionCookie(idToken);
+// //   } catch (error: any) {
+// //     console.log("");
+
+// //     return {
+// //       success: false,
+// //       message: "Failed to log into account. Please try again.",
+// //     };
+// //   }
+// // }
+
+// // // Sign out user by clearing the session cookie
+// // export async function signOut() {
+// //   const cookieStore = await cookies();
+
+// //   cookieStore.delete("session");
+// // }
+
+// // // Get current user from session cookie
+// // export async function getCurrentUser(): Promise<User | null> {
+// //   const cookieStore = await cookies();
+
+// //   const sessionCookie = cookieStore.get("session")?.value;
+// //   if (!sessionCookie) return null;
+
+// //   try {
+// //     const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+// //     // get user info from db
+// //     const userRecord = await db
+// //       .collection("users")
+// //       .doc(decodedClaims.uid)
+// //       .get();
+// //     if (!userRecord.exists) return null;
+
+// //     return {
+// //       ...userRecord.data(),
+// //       id: userRecord.id,
+// //     } as User;
+// //   } catch (error) {
+// //     console.log(error);
+
+// //     // Invalid or expired session
+// //     return null;
+// //   }
+// // }
+
+// // // Check if user is authenticated
+// // export async function isAuthenticated() {
+// //   const user = await getCurrentUser();
+// //   return !!user;
+// // }
+
+
+
+"use server";
+
+import { auth, db } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+// Session duration (1 week)
+const SESSION_DURATION = 60 * 60 * 24 * 7;
+
+// Set session cookie
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  // Create session cookie
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: SESSION_DURATION * 1000, // milliseconds
+  });
+
+  // Set cookie in the browser
+  cookieStore.set("session", sessionCookie, {
+    maxAge: SESSION_DURATION,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
 }
 
-export async function setSessionCookie(idToken: string) {
-    const cookieStore = await cookies();
+export async function signUp(params: SignUpParams) {
+  const { uid, name, email } = params;
 
-    const setSessionCookie = await auth.createSessionCookie(idToken, {
-        expiresIn: ONE_WEEK * 1000 
-    })
+  try {
+    // check if user exists in db
+    const userRecord = await db.collection("users").doc(uid).get();
+    if (userRecord.exists)
+      return {
+        success: false,
+        message: "User already exists. Please sign in.",
+      };
 
-    cookieStore.set('session', setSessionCookie, {
-        maxAge: ONE_WEEK,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        path: '/',
-        sameSite: 'lax'
-    })
+    // save user to db
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+      // profileURL,
+      // resumeURL,
+    });
+
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in.",
+    };
+  } catch (error: any) {
+    console.error("Error creating user:", error);
+
+    // Handle Firebase specific errors
+    if (error.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message: "This email is already in use",
+      };
+    }
+
+    return {
+      success: false,
+      message: "Failed to create account. Please try again.",
+    };
+  }
 }
 
 export async function signIn(params: SignInParams) {
-    const { email, idToken } = params;
-    try {
-        const userRecord = await auth.getUserByEmail(email);
+  const { email, idToken } = params;
 
-        if(!userRecord) {
-            return {
-                success: false,
-                message: 'User does not exist. Create an account instead.'
-            }
-        }
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord)
+      return {
+        success: false,
+        message: "User does not exist. Create an account.",
+      };
 
-        await setSessionCookie(idToken);
-    } catch (e) {
-        console.log(e);
+    await setSessionCookie(idToken);
+  } catch (error: any) {
+    console.log("");
 
-        return {
-            success: false,
-            message: 'Failed to log into an account.'
-        }
-    }
+    return {
+      success: false,
+      message: "Failed to log into account. Please try again.",
+    };
+  }
 }
 
+// Sign out user by clearing the session cookie
+export async function signOut() {
+  const cookieStore = await cookies();
+
+  cookieStore.delete("session");
+}
+
+// Get current user from session cookie
 export async function getCurrentUser(): Promise<User | null> {
-    const cookieStore = await cookies();
-  
-    const sessionCookie = cookieStore.get("session")?.value;
-    if (!sessionCookie) return null;
-  
-    try {
-      const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
-  
-      // get user info from db
-      const userRecord = await db
-        .collection("users")
-        .doc(decodedClaims.uid)
-        .get();
-      if (!userRecord.exists) return null;
-  
-      return {
-        ...userRecord.data(),
-        id: userRecord.id,
-      } as User;
-    } catch (error) {
-      console.log(error);
-  
-      // Invalid or expired session
-      return null;
-    }
+  const cookieStore = await cookies();
+
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+    // get user info from db
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.log(error);
+
+    // Invalid or expired session
+    return null;
   }
+}
 
+// Check if user is authenticated
 export async function isAuthenticated() {
-    const user = await getCurrentUser();
-
-    return !!user;
+  const user = await getCurrentUser();
+  return !!user;
 }
